c3cd755c50ce129f57f48e6228f51d5a3c04c044
Matthew Clower <matthew.clower@wepay.com>
2014-10-11 12:51:19 -0700

    updated Suit test to verify constants

--- lib/SuitTest.rb
+++ lib/SuitTest.rb
@@ -8,6 +8,10 @@ class SuitTest < RubyUnit::TestCase
   def invalidSuitsTest suit
     assertNot Suit::valid?(suit), "#{suit} should not be a valid Suit"
   end
+
+  def constantTest constant, value
+    assertConst value, Suit, constant, "Suit constant #{constant} is not the right value!"
+  end
 end
 
 # Extend the class to add data providing functions
@@ -27,6 +31,20 @@ class SuitTest
       [   1.5],
       [  4242],
       ['text'],
+    ]
+  end
+
+  def constantData
+    [
+      [  'SPADES',         0],
+      [  'HEARTS',         1],
+      ['DIAMONDS',         2],
+      [   'CLUBS',         3],
+      [   'SPADE',       '♠'],
+      [   'HEART',       '♥'],
+      [ 'DIAMOND',       '♦'],
+      [    'CLUB',       '♣'],
+      [   'SUITS', [0,1,2,3]],
     ]
   end
 end
